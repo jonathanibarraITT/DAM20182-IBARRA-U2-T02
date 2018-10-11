5128f810422dfe38e4d392561cde903446b965fc
jonathanibarraITT <joguibarrama@ittepic.edu.mx>
2018-10-10 19:26:33 -0600

    first commit

---
+++ src/components/foo.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+    selector:'my-component',
+    templateUrl:'foo.html'
+})
+export class MyComponent{
+    public data: any = {myToggle: true};
+
+    constructor(){}
+    isClicked(val){
+        console.log('vegetariana' + val);
+    }
+}
